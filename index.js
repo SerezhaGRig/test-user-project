d91082056acc1c7d3b5c85a327f827bf5b3e140a
Serezha <serojjan2000@gmail.com>
2021-10-18 15:57:41 +0400

    Style change.

--- index.js
+++ index.js
@@ -9,4 +9,5 @@ app.use(ctx => {
 });
 
 app.listen(port,function () {
-    console.log(`Example app listening at http://localhost:${port}`)})
+    console.log(`Example app listening at http://localhost:${port}`)
+})
