77aff5a9631862a3220e51721afeda6d9b2e15a2
Serezha <serojjan2000@gmail.com>
2021-10-18 17:46:04 +0400

    user registration

--- index.js
+++ index.js
@@ -2,12 +2,35 @@
 
 const Koa = require('koa');
 const app = new Koa();
-const port = 1234
+const bodyParser = require('koa-bodyparser');
+let Router = require('koa-router');
+const port = 8080
 
-app.use(ctx => {
-    ctx.body = 'Hello World';
-});
+let users = []
+
+app.use(bodyParser());
+
+var router = Router();
+router.get('/', (ctx, next) => {
+    ctx.body = 'Hello World!';
+})
+
+router.post('/register', (ctx, next) => {
+    users.push(ctx.request.body)
+    ctx.body = "Congratulations"
+})
+router.post('/login', (ctx, next) => {
+    let user = ctx.request.body;
+    if(users.find((element)=>(element.login===user.login && element.password === user.password)))
+        ctx.body = 'Hello World!';
+    else
+        ctx.body = 'Unauthorized';
+})
 
 app.listen(port,function () {
     console.log(`Example app listening at http://localhost:${port}`)
 })
+app
+    .use(router.routes())
+    .use(router.allowedMethods());
+
