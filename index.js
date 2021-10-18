31130bd23a8e14f111730bf2bc5d2fd65909205a
Serezha <serojjan2000@gmail.com>
2021-10-18 14:58:37 +0400

    Url logging.

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 
 const Koa = require('koa');
 const app = new Koa();
+const port = 1234
 
 app.use(ctx => {
     ctx.body = 'Hello World';
 });
 
-app.listen(1234);
+app.listen(port,function () {
+    console.log(`Example app listening at http://localhost:${port}`)})
