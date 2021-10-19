84f8cab8ce59b2bec53a22406aebb25fd6b769b1
Serezha <serojjan2000@gmail.com>
2021-10-19 17:39:40 +0400

    validation

--- services/users/index.js
+++ services/users/index.js
@@ -1,13 +1,24 @@
 const User = require('../../model/model')
+var validator = require("email-validator");
+var passwordValidator = require("password-validator");
+var schema = new passwordValidator();
+
 
 let users = []
 
-
+schema
+    .is().min(8)
 class Service{
     static hello(){
         return 'Hello World!';
     }
     static register(user_data){
+        if(!validator.validate(user_data.login)){
+            return "Input value wasn't email"
+        }
+        if(!schema.validate(user_data.password)){
+            return "Password minimum length is 8"
+        }
         let user = User.userFromJs(user_data)
         users.push(user)
         return "Congratulation"
