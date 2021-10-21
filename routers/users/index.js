d27236b1b3c96aff87caf7f39f20630c24c700d8
Serezha <serojjan2000@gmail.com>
2021-10-21 13:31:37 +0400

    error and response middlware

--- routers/users/index.js
+++ routers/users/index.js
@@ -2,6 +2,7 @@ const Router = require('koa-router');
 const Controller=require('../../controller/users/index')
 const validator = require("email-validator");
 const {jwtValidate} = require('../../services/auth/jwt')
+const {authMiddleware,responseMiddleware,errorMiddleware}= require('../../middleware/middleware')
 const passwordValidator = require("password-validator");
 
 const router = new Router();
@@ -9,44 +10,35 @@ const schema = new passwordValidator();
 schema.is().min(8)
 
 router.get('/',authMiddleware, (ctx, next) => {
-    ctx.body = Controller.hello();
-},)
+    ctx.myData = Controller.hello();
+    next()
+},responseMiddleware,errorMiddleware)
 router.post('/register', (ctx, next) => {
     const { body:{ login, password, username } } = ctx.request;
     if(!validator.validate(login)){
-        ctx.body = "Input value wasn't email"
+        ctx.myData = "Input value wasn't email"
+        next()
     }
     if(!schema.validate(password)){
-        ctx.body = "Password minimum length is 8"
+        ctx.myData = "Password minimum length is 8"
+        next()
     }
-    ctx.body = Controller.register({login,  password, username });
-},)
+    ctx.myData = Controller.register({login,  password, username });
+    next()
+},responseMiddleware,errorMiddleware)
 router.post('/login', (ctx, next) => {
     const { body:{ login, password } } = ctx.request;
-    ctx.body = Controller.login({login, password});
+    ctx.myData = Controller.login({login, password});
+    next()
 
-},)
+},responseMiddleware,errorMiddleware)
 router.post('/rename',authMiddleware, (ctx, next) => {
     const { body:{ newName } } = ctx.request;
-    ctx.body = Controller.rename({login:ctx.request.sub, newName});
-},)
+    ctx.myData = Controller.rename({login:ctx.request.sub, newName});
+    next()
+},responseMiddleware,errorMiddleware)
 
-function authMiddleware(ctx, next) {
-    const authHeader = ctx.request.headers.authorization
-    if (authHeader) {
-        const token = authHeader.slice(7);
-        try{
-            const {sub} = jwtValidate(token)
-            ctx.request.sub = sub;
-            next();
-        }catch (e) {
-            ctx.status=403;
-        }
 
-    } else {
-        ctx.status=401;
-    }
 
-}
 
 module.exports = router
